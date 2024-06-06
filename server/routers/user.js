6574ed08ccdb5ddad9f4e06ee1dff506bc4e70ce
Esther Orieji <oriejiestherr@gmail.com>
2024-06-07 00:11:07 +0100

    first

--- server/routers/user.js
+++ server/routers/user.js
@@ -1,18 +1,16 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/userController')
-
+const userController = require("../controllers/userController");
 
 // router
-router.get('/', userController.view)
-router.post('/', userController.find)
-router.get('/adduser', userController.new)
-router.post('/adduser', userController.create)
-router.get('/edituser/:id', userController.edit)
+router.get("/", userController.view);
+router.post("/", userController.find);
+router.get("/adduser", userController.new);
+router.post("/adduser", userController.create);
+router.get("/edituser/:id", userController.edit);
 
-router.post('/edituser/:id', userController.updateuser)
-router.get('/viewuser/:id', userController.watch)
-router.get('/:id', userController.delete)
+router.post("/edituser/:id", userController.updateuser);
+router.get("/viewuser/:id", userController.watch);
+router.get("/:id", userController.delete);
 
-
-module.exports = router
+module.exports = router;
